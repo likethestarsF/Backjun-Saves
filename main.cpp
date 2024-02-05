6be98d666ac6fbeab8e034e9554a47df818d132b
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-02-05 08:07:27 +0000

    240205 4 #1654 Timeout; not by overflow

--- main.cpp
+++ main.cpp
@@ -1,34 +1,74 @@
-// 240205 4 #1436
+// 240205 4 #1654
 #include <algorithm>
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
+unsigned long long findN(const vector<int> &line, int length) {
+  unsigned long long sum = 0;
+  for (const int &elem : line)
+    sum += (unsigned long long)(elem / length);
+  return sum;
+}
 void body() {
   // input
-  int n;
-  cin >> n;
+  unsigned int k, n;
+  cin >> k >> n;
 
-  //  process
-  int cnt = 0;
-  int answer = 0;
+  int maxOfLine = 0;
+  vector<int> line(k);
+  for (int i = 0; i < k; i++) {
+    cin >> line[i];
+    if (maxOfLine < line[i]) // find max length of line.
+      maxOfLine = line[i];
+  }
 
-  // Bruteforce for 666.
-  for (int i = 666;; i++) {
-    // find 666 in string i.
-    string iStr = to_string(i);
-    if (iStr.find("666") != string::npos)
-      cnt++;
+  /* n is target we need maximum length of line.
+  ** is there any mathematical method? I think, it's not.
+  ** what we can do now is brute force or much smarter way than that.
+  */
 
-    // exit condition.
-    if (cnt == n) {
-      answer = i;
+  // we find n according to length.
+  // we modify length based on n value by Binary Search.
+  // if the result is bigger than n, we increase length.
+  // in opposite case, we decrease length.
+  // and repeat this process until result and n are same.
+  long rLength = maxOfLine;
+  long lLength = 1;
+  long midLength = (rLength - lLength) / 2;
+  while (true) {
+    unsigned long long nSearch = findN(line, midLength);
+
+    // increase length and update length.
+    if (nSearch > n) {
+      lLength = midLength;
+      midLength = (rLength - lLength) / 2;
+    }
+    // decrease length and update length.
+    else if (nSearch < n) {
+      lLength = midLength;
+      midLength = (rLength - lLength) / 2;
+    }
+    // n == result(midLength).
+    else {
       break;
     }
   }
+
+  // we have to find biggest length.
+  // when result and n are same,
+  // we increase length by 1 until n and result is different.
+  // if we decrease by 1 to result, that is the answer.
+  while (true) {
+    midLength++;
+    if (n != findN(line, midLength))
+      break;
+  }
+  midLength--;
+
   // output
-  cout << answer;
+  cout << midLength;
 }
 
 int main() {
