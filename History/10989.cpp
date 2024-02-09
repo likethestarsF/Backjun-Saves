eab6dbcd06cd9cf3726b8af38ca3b4c53a5051b8
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-02-09 06:36:57 +0000

    240209 3 #10989 Solved; Counting Sort
    cf. Internet. only the concept of Counting Sort.

--- History/10989.cpp
+++ History/10989.cpp
@@ -1,7 +1,8 @@
 // 240209 3 #10989
 /* Using STL sort is the easiest way.
 ** However, because of the memory restriction we cannot apply STL sort.
-** Use Bubble Sort for It uses less memory.
+** Use Counting Sort.
+** the problem's condition is very unique.
 */
 
 #include <algorithm>
@@ -11,23 +12,23 @@ using namespace std;
 
 void body() {
   // input
+  int maxNum = 10000;
   int n;
   cin >> n;
 
-  vector<short> list(n);
+  vector<int> list(maxNum, 0);
   for (int i = 0; i < n; i++) {
-    cin >> list[i];
+    int inputInt;
+    cin >> inputInt;
+    list[inputInt - 1]++;
   }
 
-  // sorting process by Bubble Sort.
-  for (int i = 0; i < n - 1; i++)
-    for (int j = 0; j < n - 1 - i; j++)
-      if (list[j] > list[j + 1])
-        swap(list[j], list[j + 1]);
-
   // output
-  for (int i = 0; i < n; i++) {
-    cout << list[i] << '\n';
+  for (int i = 0; i < maxNum; i++) {
+    while (list[i] > 0) {
+      cout << i + 1 << '\n';
+      list[i]--;
+    }
   }
 }
 
