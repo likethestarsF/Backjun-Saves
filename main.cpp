c6d931128425f6f5099f909ca22f53df0e3d88f8
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-02-09 05:38:30 +0000

    240209 3 #10989 Memory; because of the condition of the problem, STL sort cannot be applied.

--- main.cpp
+++ main.cpp
@@ -1,40 +1,27 @@
-// 240209 2 #1676
-/* count how many factor 10 exists.
-** just count the number of 2 and 5 each.
-**and find the minimum of count of those.
-*/
+// 240209 3 #10989
+/* Using STL sort is the easiest way.
+ */
 #include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
 using namespace std;
-
-int countFactor(int target, int divisor) {
-  int sum = 0;
-  while (target % divisor == 0) {
-    sum++;
-    target /= divisor;
-  }
-  return sum;
-}
 
 void body() {
   // input
   int n;
   cin >> n;
-
-  int numOf2 = 0;
-  int numOf5 = 0;
-  for (int i = 1; i <= n; i++) {
-    // count numOf2
-    numOf2 += countFactor(i, 2);
-    // count numOf5
-    numOf5 += countFactor(i, 5);
+  vector<int> list(n);
+  for (int i = 0; i < n; i++) {
+    cin >> list[i];
   }
 
+  //  process
+  sort(list.begin(), list.end());
+
   // output
-  int numOf10 = min(numOf2, numOf5);
-  cout << numOf10;
+  for (const auto &elem : list) {
+    cout << elem << '\n';
+  }
 }
 
 int main() {
