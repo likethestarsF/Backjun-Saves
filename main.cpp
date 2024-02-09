c126a90800793ae4e1f12bde2dc271ec8f7acfa2
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-02-09 05:48:03 +0000

    240209 3 #10989 Timeout; Linear Search causes timeout.

--- main.cpp
+++ main.cpp
@@ -1,22 +1,36 @@
 // 240209 3 #10989
 /* Using STL sort is the easiest way.
- */
+** However, because of the memory restriction we cannot apply STL sort.
+** we should input the data and do sorting simultaneously.
+*/
 #include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
 void body() {
+  vector<int> list;
+
   // input
   int n;
   cin >> n;
-  vector<int> list(n);
   for (int i = 0; i < n; i++) {
-    cin >> list[i];
-  }
+    int inputInt;
+    cin >> inputInt;
 
-  //  process
-  sort(list.begin(), list.end());
+    // sorting process.
+    bool flag = true;
+    for (int i = 0; i < list.size(); i++) {
+      if (inputInt <= list[i]) {
+        list.insert(list.begin() + i, inputInt);
+        flag = false;
+        break;
+      }
+    }
+
+    if (flag)
+      list.push_back(inputInt);
+  }
 
   // output
   for (const auto &elem : list) {
