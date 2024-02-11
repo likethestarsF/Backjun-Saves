31986bfd57f1152bbe0785c112b731cb09c65cf9
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-02-11 04:19:05 +0000

    re-organize the directory by class

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// date num #eg
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void body() {
+  // input
+
+  //  process
+
+  // output
+}
+
+int main() {
+  /* cin optimize */
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  /* clog switch */
+  // std::clog.setstate(std::ios_base::failbit);
+
+  body();
+}
