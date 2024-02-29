12cdf05044c39335e1bb7e3b1d951e909649c7aa
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-02-29 09:58:28 +0000

    240229 1 #5430 AccessEmptyContainer; Seems `nList.clear()` is the reason.

--- main.cpp
+++ main.cpp
@@ -1,33 +1,88 @@
-// 240227 1 #2579
+// 240227 2 #5430
+// 240229 1 #5430
 #include <algorithm>
+#include <deque>
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
 
 class my {
-  vector<int> score;
-  vector<int> dp;
+  string command;
+  int numOfArr;
+  deque<int> nList;
+  string arr;
+  bool isReversed = false;
+  bool isError = false;
 
 public:
-  void input(int n) {
-    score = vector<int>(n);
-    dp = vector<int>(n);
-    for (int i = 0; i < n; i++) {
-      cin >> score[i];
+  void input() {
+    // init constants.
+    nList.clear();
+    isReversed = false;
+    isError = false;
+
+    cin >> command;
+    cin >> numOfArr;
+    cin >> arr;
+
+    // transfer string arr to deque<int>
+    string cache = {};
+    for (const auto c : arr) {
+      if (c != ',' && c != '[' && c != ']') {
+        cache += c;
+      } else {
+        if (!cache.empty())
+          nList.push_back(stoi(cache));
+        // clog << cache << endl;
+        cache = {};
+      }
     }
   }
 
-  void body() {
-    dp[0] = score[0];
-    dp[1] = max(score[0] + score[1], score[1]);
-    dp[2] = max(score[0] + score[2], score[1] + score[2]);
+  void exeCommand() {
+    for (const auto c : command) {
+      if (c == 'R') {
+        isReversed = !isReversed;
+      } else if (c == 'D') { // 'D': pop the first elem
+        if (nList.empty()) {
+          isError = true;
+          break; // EXIT: for loop
+        }
+        if (isReversed) {
+          nList.pop_back();
+        } else {
+          nList.pop_front();
+        }
+      } else
+        continue;
+    }
+  }
 
-    for (int i = 3; i < score.size(); i++) {
-      dp[i] = score[i] + max(dp[i - 3] + score[i - 1], dp[i - 2]);
+  void output() {
+    if (isError)
+      cout << "error" << '\n';
+
+    else if (isReversed) {
+      cout << '[';
+
+      for (int i = nList.size() - 1; i > 0; i--) {
+        cout << nList[i];
+        cout << ',';
+      }
+      cout << nList.front();
+      cout << "]\n";
     }
 
-    cout << dp.back();
+    else {
+      cout << '[';
+
+      for (int i = 0; i < nList.size() - 1; i++) {
+        cout << nList[i];
+        cout << ',';
+      }
+      cout << nList.back();
+      cout << "]\n";
+    }
   }
 };
 
@@ -41,8 +96,11 @@ int main() {
   // std::clog.setstate(std::ios_base::failbit);
 
   my a;
-  int n;
-  cin >> n;
-  a.input(n);
-  a.body();
+  int t;
+  cin >> t;
+  while (t--) {
+    a.input();
+    a.exeCommand();
+    a.output();
+  }
 }
