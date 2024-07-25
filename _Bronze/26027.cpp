5e909ffe5260788c8479cf93e253d27e2e7f8438
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-07-25 11:27:13 +0000

    240725 1 #26027 Solved;

--- _Bronze/26027.cpp
+++ _Bronze/26027.cpp
@@ -2,6 +2,7 @@
 // Random Marthon 8 B
 #include <algorithm>
 #include <iostream>
+#include <math.h>
 #include <string>
 #include <vector>
 using namespace std;
@@ -12,24 +13,8 @@ class my {
 public:
   void body() {
     cin >> r;
-    const long long int sq = r * r;
 
-    int xAnswer = -1, yAnswer = -1;
-
-    for (int x = r - 1; x > 0; x--) {
-      for (int y = 1; y <= r; y++) {
-        long long int tempSq = x * x + y * y;
-
-        // find x^2 + y^2 = r^2 + 1
-        if (tempSq == sq + 1) {
-          xAnswer = x;
-          yAnswer = y;
-          goto END;
-        }
-      }
-    }
-  END:
-    cout << xAnswer << ' ' << yAnswer;
+    cout << r << ' ' << 1;
   }
 };
 
