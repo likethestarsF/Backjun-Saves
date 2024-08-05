37226dadc2e9e2769a6592f179843e30f920169d
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-08-05 13:11:47 +0000

    240805 1 #15663;
    How to avoid the duplication?

---
+++ Class_4/15663.cpp
@@ -0,0 +1,60 @@
+// 240805 1 #15663
+// Class 4
+// 00:
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class my {
+  int N, M;
+  vector<int> num;
+
+  vector<int> answer;
+  void backtracking(int depth) {
+    // Exit
+    if (depth == M) {
+      for (const int &elem : answer)
+        cout << elem << ' ';
+      cout << '\n';
+    }
+
+    else {
+      for (int i = 0; i < N; i++) {
+        // Select
+        answer[depth] = num[i];
+
+        backtracking(depth + 1);
+
+        // Deselect
+      }
+    }
+  }
+
+public:
+  void body() {
+    // #input
+    cin >> N >> M; // [1, 8], M <= N
+    num.resize(N);
+    for (int i = 0; i < N; i++)
+      cin >> num[i]; // [1, 10000]
+    sort(num.begin(), num.end());
+
+    // sorted by inc. dictionary order
+    answer.resize(M);
+    backtracking(0);
+  }
+};
+int main() {
+  /* cin optimize */
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  /* clog switch */
+  // std::clog.setstate(std::ios_base::failbit);
+
+  my a;
+  a.body();
+}
