6cd11bca854e1c9fd449e1a94ea341e82cd9862e
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-08-09 04:27:54 +0000

    240809 1 #24927 solved;

--- _Bronze/24927.cpp
+++ _Bronze/24927.cpp
@@ -1,6 +1,6 @@
 // 240809 1 #24927
 // Random Marathon 10 D
-// 00:00
+// 00:15
 #include <algorithm>
 #include <iostream>
 #include <vector>
@@ -8,7 +8,6 @@ using namespace std;
 
 class my {
   int numSize, K;
-  bool isPossible = false;
 
 public:
   void body() {
@@ -17,18 +16,16 @@ public:
       int x;
       cin >> x; // [1, 10^9]
 
-      if (K == 0) {
-        isPossible = true;
-        continue;
-      }
-
       while (x % 2 == 0) {
         x /= 2;
         K--;
       }
     }
 
-    cout << (int)isPossible;
+    if (K > 0)
+      cout << 0 << '\n';
+    else
+      cout << 1 << '\n';
   }
 };
 
