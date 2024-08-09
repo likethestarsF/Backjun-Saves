9df719f7ce88f992f0cf75ac6bf24666b13e686d
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-08-09 04:44:29 +0000

    240809 2 #4118 wrong;

---
+++ _Bronze/4118.cpp
@@ -0,0 +1,59 @@
+// 240809 2 #4118
+// Random Marathon 10 C
+// 00:00
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class my {
+  int ticketSize;
+  vector<bool> isBought;
+
+  bool IsAllPurchased() {
+    isBought.resize(50, false); // [0] isn't used
+
+    // input Lotto numbers
+    for (int i = 0; i < ticketSize; i++) {
+      for (int j = 0; j < 6; j++) {
+        int temp;
+        cin >> temp;
+        isBought[temp] = true;
+      }
+    }
+
+    // Check if everything is purchased
+    for (int i = 1; i < 50; i++)
+      if (isBought[i] == false)
+        return false;
+
+    return true;
+  }
+
+public:
+  void body() {
+    while (true) {
+      cin >> ticketSize; // [1, 100]
+      if (ticketSize == 0)
+        return;
+
+      if (IsAllPurchased())
+        cout << "Yes\n";
+      else
+        cout << "No\n";
+    }
+  }
+};
+
+int main() {
+  /* cin optimize */
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  /* clog switch */
+  // std::clog.setstate(std::ios_base::failbit);
+
+  my a;
+  a.body();
+}
