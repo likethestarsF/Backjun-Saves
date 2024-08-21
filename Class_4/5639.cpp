db8b5d6e4880c7c253bafbc75bfd220ebe127bd9
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-08-21 00:03:32 +0000

    240821 1 #5639;
    it seems need to use the Linked list. array based method is hard to make as it's difficult to predict the array size.

--- Class_4/5639.cpp
+++ Class_4/5639.cpp
@@ -1,20 +1,37 @@
 // 240820 2 #5639
+// 240821 1 #5639
 // Class 4
 // 00:00
 #include <algorithm>
 #include <iostream>
-#include <queue>
 #include <vector>
+#define MAX 1000000000
 using namespace std;
 
 class my {
+  vector<int> elems = {};
   vector<int> tree;
+
   // left = root*2, right = root*2+1
 
   void swap(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
+  }
+
+  void Insertion(const int &key, const int &rootIdx) {
+    if (tree[rootIdx] == -1) {
+      tree[rootIdx] = key;
+      return;
+    }
+
+    if (key > tree[rootIdx])
+      Insertion(key, rootIdx * 2 + 1);
+    else if (key < tree[rootIdx])
+      Insertion(key, rootIdx * 2);
+    else
+      return;
   }
   void Sorting() {
     const int maxIdx = tree.size() - 1;
@@ -42,24 +59,27 @@ class my {
 
 public:
   void body() {
-    // Idx of Binary tree starts from 1
-    tree.resize(1, -1);
-
     /* Input */
     while (true) {
       int input;
       cin >> input;
-      if (cin.eof())
-        break;
-      tree.push_back(input);
-      Sorting();
 
       // test exit
       if (input == -1)
         break;
+
+      if (cin.eof())
+        break;
+      elems.push_back(input);
     }
     /* Main */
-    // make the
+    // Idx of Binary tree starts from 1
+    tree.resize(10, -1);
+    tree[0] = -1;
+    // make the tree
+    for (int i = 1; i < elems.size() + 1; i++)
+      // Insertion(elems[i], 1)
+      ;
   }
 };
 
