fdb307be97c538e141ab877729e1f97df9b5b018
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-09-08 05:01:13 +0000

    240908 1 #2467 solved; Binary Search
    One is selected sequentially, and the other is selected by Binary search.
    Error happened because initial value for minDiff was too small before. forgot to multiply 2 after rewriting.
    
    Instead of the Binary Seach, 2-Pointer can be used also; one starting from the max, and the other starting from the min, moving one by one based on a sum of both.

--- Class_5/2467.cpp
+++ Class_5/2467.cpp
@@ -5,52 +5,12 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-#define ll long long
 using namespace std;
 
 class my {
   int N;
-  int minDiff = 1000000001;
+  int minDiff = 2000000001;
   pair<int, int> selected = {0, 0};
-
-  int binarySearch(const int &pos, const vector<int> &list) {
-    int lbd = 0, rbd = list.size() - 1;
-    int minDiff = pos;
-    int minElem = -1;
-
-    while (lbd <= rbd) {
-      int mid = (lbd + rbd) / 2; // left one is selected always
-      int diff = pos + list[mid];
-      if (minDiff > abs(diff)) {
-        minDiff = abs(diff);
-        minElem = list[mid];
-      }
-
-      if (diff == 0)
-        return list[mid];
-
-      else if (diff > 0) {
-        /*
-        pos is bigger than neg from list
-        -> b should be bigger
-        -> lb should be mid + 1.
-        */
-        lbd = mid + 1;
-
-      }
-
-      else {
-        /*
-        neg from list is bigger than a
-        -> b should be smaller
-        -> rb should be mid - 1.
-        */
-        rbd = mid - 1;
-      }
-    }
-
-    return minElem;
-  }
 
   void updateGlobalMin(const int &first, const int &second) {
     int diff = first + second;
