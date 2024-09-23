680535a986f0a1c4634a3176256d8d1739361626
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-09-23 23:08:11 +0000

    240924 1 #1202 wrong;

---
+++ Class_5/1202.cpp
@@ -0,0 +1,69 @@
+// 240924 1 #1202
+// Class 5
+// 00:00
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <queue>
+#include <vector>
+#define MAX 2147483647
+using namespace std;
+
+class MY {
+  struct jewel {
+    int weight, value;
+
+    static bool compare(const jewel &a, const jewel &b) {
+      return (double)a.value / a.weight > (double)b.value / b.weight;
+    }
+  };
+
+  int jewelNum, bagNum;    // [,3e5]
+  vector<int> bag;         // [,1e8]
+  vector<jewel> jewelList; // [,1e6]
+
+public:
+  MY() {
+    /* Input */
+    cin >> jewelNum >> bagNum;
+    jewelList.resize(jewelNum);
+    bag.resize(bagNum);
+
+    for (int i = 0; i < jewelNum; i++)
+      cin >> jewelList[i].weight >> jewelList[i].value;
+    for (int i = 0; i < bagNum; i++)
+      cin >> bag[i];
+  }
+
+  void body() {
+    // 'value/weight': decreasing order
+    sort(jewelList.begin(), jewelList.end(), jewel::compare);
+
+    long long maxValue = 0; // 3e5*1e6 > 2^31
+    for (int i = 0; i < bagNum; i++) {
+      // select a jewel for the bag
+      for (int idxJ = 0; idxJ < jewelList.size(); idxJ++) {
+        if (bag[i] >= jewelList[idxJ].weight) {
+          maxValue += jewelList[idxJ].value;
+          jewelList.erase(jewelList.begin() + idxJ);
+          break;
+        }
+      }
+    }
+
+    cout << maxValue;
+  }
+};
+
+int main() {
+  /* cin optimize */
+  ios_base ::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  /* clog switch */
+  // std::clog.setstate(std::ios_base::failbit);
+
+  MY my;
+  my.body();
+}
