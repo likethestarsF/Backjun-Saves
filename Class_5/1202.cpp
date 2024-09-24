d8d1f05db83649b7fdbd173353fc51b0203c17b7
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-09-24 00:01:37 +0000

    240924 1 #1202 sovled; Priority Queue

--- Class_5/1202.cpp
+++ Class_5/1202.cpp
@@ -1,26 +1,33 @@
 // 240924 1 #1202
 // Class 5
-// 00:00
+// 01:30
 #include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <queue>
 #include <vector>
 #define MAX 2147483647
+#define ll long long
 using namespace std;
 
 class MY {
   struct jewel {
     int weight, value;
 
-    static bool compare(const jewel &a, const jewel &b) {
-      return a.value / a.weight > b.value / b.weight;
+    static bool weightComp(const jewel &a, const jewel &b) {
+      return a.weight < b.weight;
+    }
+  };
+
+  struct valueComp {
+    bool operator()(const jewel &a, const jewel &b) {
+      return a.value < b.value;
     }
   };
 
   int jewelNum, bagNum;    // [,3e5]
   vector<int> bag;         // [,1e8]
-  vector<jewel> jewelList; // [,1e6]
+  vector<jewel> jewelList; // [0, 1e6]
 
 public:
   MY() {
@@ -36,23 +43,42 @@ public:
   }
 
   void body() {
-    // 'value/weight': decreasing order
-    sort(jewelList.begin(), jewelList.end(), jewel::compare);
+    sort(jewelList.begin(), jewelList.end(), jewel::weightComp);
     sort(bag.begin(), bag.end());
 
-    long long maxValue = 0; // 3e5*1e6 > 2^31
+    priority_queue<jewel, vector<jewel>, valueComp> pq;
+
+    /** Priority Queue
+     * 1. Push everything that can be handled by current bag.
+     * * idx is remains even if the while() loop is ended;
+     * * already pushed elems don't need to be pushed again.
+
+     * 2. Pop the top. add its value to the sum.
+     * * Since queue is sorted by the value, top is always the most valuable
+     * * and the bag is sorted by weight, jewels in the queue that
+     * * previous bags can handle also can be carried by latter bags.
+     */
+
+    ll sumValue = 0; // 3e5*1e6 > 2^31
+    int idx = 0;
     for (int i = 0; i < bagNum; i++) {
-      // select a jewel for the bag
-      for (int idxJ = 0; idxJ < jewelList.size(); idxJ++) {
-        if (bag[i] >= jewelList[idxJ].weight) {
-          maxValue += jewelList[idxJ].value;
-          jewelList.erase(jewelList.begin() + idxJ);
+      // 1.
+      while (idx < jewelNum) {
+        if (bag[i] >= jewelList[idx].weight)
+          pq.push(jewelList[idx++]);
+        else
           break;
-        }
+      }
+
+      // 2.
+      if (!pq.empty()) {
+        const int value = pq.top().value;
+        pq.pop();
+        sumValue += value;
       }
     }
 
-    cout << maxValue;
+    cout << sumValue;
   }
 };
 
