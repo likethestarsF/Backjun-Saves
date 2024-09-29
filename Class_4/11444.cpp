7dc9304f7bf792ec6311161c0620cdb6f48e8ded
likethestarsf <31518984-likethestarsf@users.noreply.replit.com>
2024-09-29 10:53:10 +0000

    240928 1 #11444 solved; Power by Divide and Conquer
    Power by Divide and Conquer. refer to Internet.
    
    Recursion used to find fibonacci.

--- Class_4/11444.cpp
+++ Class_4/11444.cpp
@@ -9,25 +9,39 @@
 using namespace std;
 
 class MY {
+  // def matrixBasic
+  const vector<vector<ll>> matrixBasic = {{1, 1}, {1, 0}};
+  const vector<vector<ll>> matrixDiagonal = {{1, 0}, {0, 1}};
 
-  ll fibo(const ll &n) {
+  // Recursive ^n = (^n/2)^2
+  vector<vector<ll>> fibo(const ll &n) {
     if (n == 0)
-      return 0;
+      return matrixDiagonal;
     if (n == 1)
-      return 1;
+      return matrixBasic;
 
-    ll prevPrev = 0;
-    ll prev = 1;
-    ll cur;
+    vector<vector<ll>> mat = fibo(n / 2);
 
-    for (int i = 2; i <= n; i++) {
-      cur = (prev + prevPrev) % 1000000007;
+    if (n % 2 == 0)
+      return multiplication(mat, mat);
+    else
+      return multiplication(multiplication(mat, mat), matrixBasic);
+  }
 
-      // update prevs
-      prevPrev = prev;
-      prev = cur;
-    }
-    return cur;
+  vector<vector<ll>> multiplication(const vector<vector<ll>> &mat1,
+                                    const vector<vector<ll>> &mat2) {
+
+    vector<vector<ll>> result(2, vector<ll>(2));
+    result[0][0] = mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0];
+    result[0][1] = mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1];
+    result[1][0] = mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0];
+    result[1][1] = mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1];
+
+    for (int i = 0; i < 2; i++)
+      for (int j = 0; j < 2; j++)
+        result[i][j] %= 1000000007;
+
+    return result;
   }
 
 public:
@@ -38,8 +52,15 @@ public:
     ll input;
     cin >> input; // N[,1e18]
 
-    // O(N)
-    cout << fibo(input) % 1000000007;
+    /** Main
+     * ( 1 1 )^n = Fn-1 Fn
+     * ( 1 0 )     Fn Fn-2
+     */
+    auto answer = fibo(input);
+    cout << answer[0][1] % 1000000007;
+
+    // why multiply is done 1 more time?
+    // is it because of the diagonal?
   }
 };
 
