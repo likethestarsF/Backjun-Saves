ab979bc4c7b319fdff511d9b44c051a1937c5e00
likethestarsF <157440819+likethestarsF@users.noreply.github.com>
2024-12-20 11:26:52 +0000

    241220 1 #3085 solved; Brute Force
    Brute Force. with help of Internet.
    Choose a Simple way: swap and check repeatedly.

--- _Silver/3085.cpp
+++ _Silver/3085.cpp
@@ -1,6 +1,6 @@
 // 241220 1 #3085
 // Random Marathon 29 H
-// 00:40
+// 01:00
 #include <algorithm>
 #include <iostream>
 #include <queue>
@@ -13,63 +13,22 @@ using namespace std;
 class MY {
   int size;
   vector<vector<char>> board;
-  int answer = 0;
+  int answer = 1;
 
   int checkHorizonal(const int &row) {
     int maxLength = 0;
     for (int start = 0; start < size; start++) {
       const char *cur = &board[row][start];
-      bool oneCoin = true;
-
-      // check the prev to optimize
-      if (start != 0 && board[row][start - 1] == *cur)
-        continue;
 
       int curLength = 1;
-
-      // default
       for (int i = start + 1; i < size; i++) {
         if (board[row][i] == *cur)
           curLength++;
-
-        // swap is possible
-        else if (oneCoin) {
-          // 1. boundary check and swap adjacents
-          if ((row != 0 && board[row - 1][i] == *cur) ||
-              (row != size - 1 && board[row + 1][i] == *cur)) {
-            curLength++;
-            break;
-          }
-
-          // 2.jump one candy
-          else if (i + 1 < size && board[row][i + 1] == *cur) {
-            curLength++;
-            oneCoin = false;
-          }
-
-          // 3. stop
-          else
-            break;
-        }
-
         else
           break;
       }
+
       maxLength = max(maxLength, curLength);
-
-      // special case
-      if (start - 2 >= 0 && board[row][start - 2] == *cur) {
-        curLength = 2;
-
-        for (int i = start + 1; i < size; i++) {
-          if (board[row][i] == *cur)
-            curLength++;
-
-          else
-            break;
-        }
-        maxLength = max(maxLength, curLength);
-      }
     }
 
     return maxLength;
@@ -78,60 +37,54 @@ class MY {
     int maxLength = 0;
     for (int start = 0; start < size; start++) {
       const char *cur = &board[start][col];
-      bool oneCoin = true;
-
-      // check the prev to optimize
-      if (start != 0 && board[start - 1][col] == *cur)
-        continue;
 
       int curLength = 1;
-
-      // default
       for (int i = start + 1; i < size; i++) {
         if (board[i][col] == *cur)
           curLength++;
-
-        // swap is possible
-        else if (oneCoin) {
-          // 1. boundary check and swap adjacents
-          if ((col != 0 && board[i][col - 1] == *cur) ||
-              (col != size - 1 && board[i][col + 1] == *cur)) {
-            curLength++;
-            oneCoin = false;
-          }
-
-          // 2.jump one candy
-          else if (i + 1 < size && board[i + 1][col] == *cur) {
-            curLength++;
-            break;
-          }
-
-          // 3. stop
-          else
-            break;
-        }
-
         else
           break;
       }
+
       maxLength = max(maxLength, curLength);
-
-      // special case
-      if (start - 2 >= 0 && board[start - 2][col] == *cur) {
-        curLength = 2;
-
-        for (int i = start + 1; i < size; i++) {
-          if (board[i][col] == *cur)
-            curLength++;
-
-          else
-            break;
-        }
-        maxLength = max(maxLength, curLength);
-      }
     }
 
     return maxLength;
+  }
+
+  void swapBoard() {
+    /**
+     * swap
+     * check (both horizonal and vertical)
+     * restore
+     */
+    // 1. swap left to right
+    for (int r = 0; r < size; r++) {
+      for (int c = 0; c < size - 1; c++) {
+        swap(board[r][c], board[r][c + 1]);
+
+        for (int row = 0; row < size; row++)
+          answer = max(answer, checkVertical(row));
+        for (int col = 0; col < size; col++)
+          answer = max(answer, checkHorizonal(col));
+
+        swap(board[r][c], board[r][c + 1]);
+      }
+    }
+
+    // 2. swap up to down
+    for (int r = 0; r < size - 1; r++) {
+      for (int c = 0; c < size; c++) {
+        swap(board[r][c], board[r + 1][c]);
+
+        for (int row = 0; row < size; row++)
+          answer = max(answer, checkVertical(row));
+        for (int col = 0; col < size; col++)
+          answer = max(answer, checkHorizonal(col));
+
+        swap(board[r][c], board[r + 1][c]);
+      }
+    }
   }
 
 public:
@@ -147,17 +100,7 @@ public:
         cin >> board[r][c]; // C P Z Y
 
     /* Process */
-    // starting from [0][0]
-    // 1. left to right : one life to check up or down
-    // 2. top to bottom : one life to check left or right
-    // don't do check if prev was same; do only when the start
-
-    // 1. left to right
-    for (int row = 0; row < size; row++)
-      answer = max(answer, checkHorizonal(row));
-    // 2. up to down
-    for (int col = 0; col < size; col++)
-      answer = max(answer, checkVertical(col));
+    swapBoard();
 
     /* Output */
     cout << answer; // maxLength of a line
